8fcede113cf85bd827913276409fa99e9e90dd18
Valentino Saitz <valentino.saitz@optris.de>
2025-12-22 12:27:58 +0100

    Added icons

---
+++ publish.bat
@@ -0,0 +1,25 @@
+﻿@echo off
+setlocal
+
+rem Read token from GithubToken.txt
+if not exist "GithubToken.txt" (
+    echo GithubToken.txt not found.
+    exit /b 1
+)
+
+for /f "usebackq delims=" %%A in ("GithubToken.txt") do (
+    set "token=%%A"
+    goto :token_read
+)
+
+:token_read
+if not defined token (
+    echo Token not found in GithubToken.txt.
+    exit /b 1
+)
+
+vpk pack --packTitle "Factor Calculator" --packId "DVSProductions.Factor-Calculator" --packAuthors "DVSProductions" --mainExe FactorCalculator.exe --noPortable --packVersion 1.0.0 --packDir bin\Release\net9.0
+
+vpk upload github --repoUrl "https://github.com/DVSProductions/Factor-Calculator" --publish --token %token%
+
+endlocal
