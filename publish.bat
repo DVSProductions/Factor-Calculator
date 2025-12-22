3d20e8c55cfc7d88937a504f2c780cbe1e7ef951
Valentino Saitz <valentino.saitz@optris.de>
2025-12-22 13:40:52 +0100

    Updated Icon to be cleaner. cleaned up the main window a lil

--- publish.bat
+++ publish.bat
@@ -1,6 +1,12 @@
 @echo off
 setlocal
-set version=1.0.2
+
+rem Prompt the user for the version
+set /p version=Enter version (e.g., 1.0.3): 
+if not defined version (
+    echo Version is required.
+    exit /b 1
+)
 
 rem Read token from GithubToken.txt
 if not exist "GithubToken.txt" (
@@ -20,8 +26,8 @@ if not defined token (
 )
 echo %token%
 pause
-dotnet publish -c Release --no-self-contained --arch x64 -f net9.0-windows10.0.17763.0  --output "bin\Velopack" --nologo --property:DebugSymbols=false --property:WarningLevel=0 --property:AnalysisLevel=0 --property:debug=none --property:PublishReadyToRun=false
 
+dotnet publish -c Release --no-self-contained --arch x64 -f net9.0-windows10.0.17763.0 --output "bin\Velopack" --nologo --property:DebugSymbols=false --property:WarningLevel=0 --property:AnalysisLevel=0 --property:debug=none --property:PublishReadyToRun=false
 
 vpk pack --packVersion %version% --packTitle "Factor Calculator" --packId "DVSProductions.Factor-Calculator" --packAuthors "DVSProductions" --mainExe FactorCalculator.exe --noPortable --packDir "bin\Velopack" --icon "Assets\Icon.ico"
 
